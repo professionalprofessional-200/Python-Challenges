855ddd79b6cb8bc3d9d813f7e3b7925beac93486
Martin Arul IrudhayaSelsiya S <professionalusage0@gmail.com>
2024-10-18 15:09:49 +0530

    ans.py

---
+++ Challenge-7/ans.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    
+    return s[::-1]
+    
+user_string=input("enter a string:")
+
+reversed_string=reverse_string(user_string)
+
+print(f"Reversed string is {reversed_string}")
