38173449fa464c69a953656597d4f8dfb7df0a7e
Martin Arul IrudhayaSelsiya S <professionalusage0@gmail.com>
2024-10-22 13:05:37 +0530

    ans.py

---
+++ Challenge-13/ans.py
@@ -0,0 +1,16 @@
+my_dict={
+    "name":"mart",
+    "age":21,
+    "department":"Python fullstack development",
+    "status":"junier"
+}
+
+print("Keys:")
+
+for key in my_dict.keys():
+    print(key)
+    
+print("\nValues:")
+
+for value in my_dict.values():
+    print(value)
